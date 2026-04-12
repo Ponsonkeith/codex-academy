1f6f826f8c31fab0f3692c1125afa7acac0e9133
Ponsonkeith <keith@doit.aw>
2026-04-12 00:54:26 -0400

    Fix next.config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,9 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '*.supabase.co' },
-      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
     ],
-  },
-  // Required for Stripe webhook (raw body)
-  api: {
-    bodyParser: false,
   },
 }
 
