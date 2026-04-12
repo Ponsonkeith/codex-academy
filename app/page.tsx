968bad9b7eb446d95770d48047a443636f8f84fa
Ponsonkeith <keith@doit.aw>
2026-04-12 02:06:57 -0400

    Redirect to HTML landing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import { redirect } from 'next/navigation'
+
 export default function Home() {
-  return (
-    <div dangerouslySetInnerHTML={{ __html: `
-      <script>window.location.href='/landing.html'</script>
-    ` }} />
-  )
+  redirect('/index.html')
 }
